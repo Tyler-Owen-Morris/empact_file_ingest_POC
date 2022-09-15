cfa3fae9f9558bc657ada81b510411634206650c
tmorris1 <tmorris@walkerinfo.com>
2022-09-15 09:36:45 -0700

    read from S3

--- lambda_function.py
+++ lambda_function.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 ## **** CONFIGURATION VARIABLES **** ##
-bucket = 'aecf-jjsf-empact-tableu'
+bucket = 'walkersftptest'
 
 # S3 INIT
 s3 = boto3.resource('s3')
@@ -22,7 +22,7 @@ my_bucket = s3.Bucket(bucket)
 ## This is the entry point for the API endpoint being called.
 def lambda_handler(event, context):
     print("function called")
-    pass
+    read_from_s3()
 
 ## UTILITY FUNCTIONS
 def read_from_s3():
